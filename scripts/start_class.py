fdaba442efeb565104b08b4e71ecda1c983f41b5
Lucas Crownover <lcrown@uoregon.edu>
2021-11-26 13:54:35 -0800

    more config

--- scripts/start_class.py
+++ scripts/start_class.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python3
 
 import requests
-
+import threading
 import config
 
 token = config.API_TOKEN
-headers = {
+HEADERS = {
     "Authorization": f"token {token}",
     "Content-Type": "application/json",
 }
-server = "http://uo-jupyterhub.westus2.cloudapp.azure.com"
+SERVER = "http://uo-jupyterhub.westus2.cloudapp.azure.com"
+
+def task(username):
+    print(f"Starting: {username} ... ", end="", flush=True)
+    requests.post(f"{SERVER}/hub/api/users/{username}/server", headers=HEADERS)
+    print("done")
 
 usernames = ['jupytertest', 'mshepard']
 
+threads = []
 for username in usernames:
-    r = requests.post(f"{server}/hub/api/users/{username}/server", headers=headers)
-    print(r.text)
+    t = threading.Thread(target=task, args=username,)
+    threads.append(t)
+
+for t in threads:
+    t.start()
+
+for t in threads:
+    t.join()
 
