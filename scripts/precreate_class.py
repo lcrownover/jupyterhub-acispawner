96d6dc829056961b980cf90e8db8f97ce7a543fe
Lucas Crownover <lcrown@uoregon.edu>
2021-11-26 17:25:46 -0800

    precreate class script

--- scripts/precreate_class.py
+++ scripts/precreate_class.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import requests
-import threading
 import json
 
 import config
@@ -12,10 +11,6 @@ HEADERS = {
     "Content-Type": "application/json",
 }
 SERVER = "http://uo-jupyterhub.westus2.cloudapp.azure.com"
-
-def task(username):
-    requests.post(f"{SERVER}/hub/api/users/{username}/server", headers=HEADERS)
-    print(f"{username}: spawning")
 
 def precreate_user(username):
     print(f"{username} doesnt exist in jupyterhub, creating")
@@ -29,17 +24,6 @@ usernames.extend([f"student{i}" for i in range(1,9)])
 
 existing_users = [user["name"] for user in json.loads(requests.get(f"{SERVER}/hub/api/users", headers=HEADERS).text)]
 
-threads = []
 for username in usernames:
     if username not in existing_users:
         precreate_user(username)
-
-    t = threading.Thread(target=task, args=(username,))
-    threads.append(t)
-
-for t in threads:
-    t.start()
-
-for t in threads:
-    t.join()
-
